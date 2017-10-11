9e7a4f03c1ac23433b822d90f0c1d9942558fd0d
Josh <josh@josh>
2017-10-11 11:42:11 -0500

    solve difference problem

---
+++ difference.py
@@ -0,0 +1,27 @@
+while True:
+    
+    try:
+        int_list = list(map(int, input().split(' ')))
+    except EOFError:
+        exit()
+    
+    iteration_count = 0
+    
+    def check_equivalent(numlist):
+        for i in range(len(numlist)):
+            if numlist[i] != numlist[0]:
+                return False
+        if numlist[0] == 0:
+            exit()
+        else:
+            return True
+        
+    while not check_equivalent(int_list):
+        new_list = []
+        for i in range(4):
+            new_list.append(abs(int_list[i] - int_list[(i + 1) % 4]))
+            
+        int_list = new_list
+        iteration_count += 1
+                
+    print(iteration_count)
