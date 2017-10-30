9f4c1d246ef6da5e6ba0c292d5ccb02599e554c5
Josh Trahan <josht0288@gmail.com>
2017-10-30 13:01:52 -0500

    actually finish palindrome problem hopefully

--- palindromes.py
+++ palindromes.py
@@ -1,3 +1,7 @@
+# South Central USA 2014 - 6795 - Palindrome Numbers
+
+import random
+
 def is_palindrome(num):
     num = str(num)
     for i in range(len(num)):
@@ -9,28 +13,41 @@ def is_palindrome(num):
 def find_greater_palindrome(num):
     if is_palindrome(num):
         return num
-    #print(num)
 
     numstr = str(num)
     if len(numstr) % 2 == 0:
-        left = numstr[:int(len(numstr) / 2)]
+        left = numstr[:len(numstr) // 2]
         mid = ''
-        right = numstr[int(len(numstr) / 2):]
+        right = numstr[len(numstr) // 2:]
+
+        if int(right) > int(left[::1]):
+            left = str(int(left) + 1)
+        right = left[::-1]
     else:
-        left = numstr[:int(len(numstr) / 2)]
-        mid = numstr[int(len(numstr) / 2)]
-        right = numstr[int(len(numstr) / 2) + 1:]
+        left = numstr[:len(numstr) // 2]
+        mid = numstr[len(numstr) // 2]
+        right = numstr[(len(numstr) // 2) + 1:]
+
+        if int(right) > int(left[::1]):
+            mid = str((int(mid) + 1) % 10)
+            left = str(int(left) + 1) if int(mid) == 0 else left
+        right = left[::-1]
+
         
-    #print(left, mid, right)
-
-    if int(right[::-1]) > int(left):
-        left = str(int(left) + 1)
-    right = int(str(left)[::-1])
-
     return str(left) + str(mid) + str(right)
 
 num_of_inputs = int(input())
-
 for i in range(num_of_inputs):
     num = int(input())
     print(find_greater_palindrome(num))
+
+# while True:
+#     randstr = ''
+#     for _ in range(random.randint(0, 80)):
+#         randstr += str(random.randint(0, 9))
+    
+#         if is_palindrome(find_greater_palindrome(randstr)):
+#             print("Success")
+#         else:
+#             print("Failure")
+#             break
